a126fdab9055a6505528a28a9fe106d31b2f94a9
balram <taryan692@gmail.com>
2025-03-29 14:08:12 +0530

    Updated user page and fixed navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
 
 function page() {
   return (
     <div className='text-center flex justify-between items-center'>
-      <h1 className='text-4xl font-bold text-red-600'> GO TO THE USERS PAGE </h1>
+    <Link href="/users">  <h1 className='text-4xl font-bold text-red-600'> GO TO THE USERS PAGE </h1></Link>
     </div>
   )
 }
